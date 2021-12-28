695d6d86f4a2aef79d5e6a04f4472cd210a9422f
james a <jamesacoding@gmail.com>
2021-12-28 18:46:57 -0500

    Loader User Experience

--- loanCalc.js
+++ loanCalc.js
@@ -1,6 +1,14 @@
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document.querySelector('#loan-form').addEventListener('submit', function(e) {
+    document.getElementById('results').style.display = 'none';
 
-function calculateResults(e) {
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+function calculateResults() {
 
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -21,15 +29,20 @@ function calculateResults(e) {
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments)- principal).toFixed(2);
 
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
+
     } else {
         showError('Please check your numbers')
     }
 
 
-    e.preventDefault();
+    
 }
 
 function showError(error) {
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
 
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading')
--- practiceLoanCal.js
+++ practiceLoanCal.js
@@ -1,7 +1,15 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.querySelector('#loan-form').addEventListener('submit', function(e) {
+    document.getElementById('results').style.display = 'none';
 
-function calculateResults(e) {
-   
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+function calculateResults() {
+
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
@@ -10,26 +18,31 @@ function calculateResults(e) {
     const totalInterest = document.getElementById('total-interest');
 
     const principal = parseFloat(amount.value);
-    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12 ;
     const calculatedPayments = parseFloat(years.value) * 12;
 
     const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-    const monthly = (principal * x * calculatedInterest) / (x - 1);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
 
     if(isFinite(monthly)) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments)- principal).toFixed(2);
+
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
+
     } else {
-        showError('Please Check Your Numbers')
+        showError('Please check your numbers')
     }
 
 
-    e.preventDefault();
+    
 }
 
-
 function showError(error) {
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
 
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading')
